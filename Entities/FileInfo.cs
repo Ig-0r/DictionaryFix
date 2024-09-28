0068d1fc4b74e923c8a4af843494ca3112c33594
Igor Leite <igorlfilgueiras@gmail.com>
2024-09-27 21:39:34 -0300

    ...

---
+++ Entities/FileInfo.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace DictionaryFix.Entities
+{
+    internal class CSVinfo
+    {
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public int Votes { get; set; }
+
+        public CSVinfo() { }
+
+        public CSVinfo(string path) 
+        {
+            Path = path;
+        }        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using DictionaryFix.Entities;
+using System.IO;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter full file path: ");
+        string path = Console.ReadLine();
+
+        Dictionary<string, int> votesInfo = new Dictionary<string, int>();
+
+        CSVinfo fileInfo = new CSVinfo(path);
+
+        try
+        {
+            using (StreamReader sr = File.OpenText(fileInfo.Path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(',');
+                    fileInfo.Name = line[0];
+                    fileInfo.Votes = int.Parse(line[1]);
+                    if (votesInfo.ContainsKey(fileInfo.Name))
+                    {
+                        votesInfo[fileInfo.Name] += fileInfo.Votes;
+                    }
+                    else
+                    {
+                        votesInfo[fileInfo.Name] = fileInfo.Votes;
+                    }
+                }
+
+                foreach (KeyValuePair<string, int> obj in votesInfo)
+                {
+                    Console.WriteLine(obj.Key + ": " + obj.Value);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+}
